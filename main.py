eb82f472c46083c8a1075c2bc33d3170c805560c
𝒩𝑜𝓏𝒾𝓁𝒶 <nozilarajabboyeva@gmail.com>
2025-11-24 15:35:36 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+#1-misol
+text = "salom dunyo  alik"
+print(f"Text: {text}")
+
+son = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        son += 1
+
+print(f"Bo'sh joylar soni: {son}")
+
+#2-misol
+text = "toshkentshahri"
+print(f"Text: {text}")
+
+son = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        son += 1
+
+print(f"Bo'sh joylar soni: {son}")
+
+#3-misol
+text = "python dasturlash tili"
+print(f"Text: {text}")
+
+son = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        son += 1
+
+print(f"Bo'sh joylar soni: {son}")
+
+#4-misol
+text = "salom dunyo!vaalik?rahmat"
+print(f"Text: {text}")
+
+son = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        son += 1
+
+print(f"Bo'sh joylar soni: {son}")
+
+#5-misol
+text = "a b c d e f g"
+print(f"Text: {text}")
+
+son = 0
+for i in range(len(text)):
+    if text[i] == " ":
+        son += 1
+
+print(f"Bo'sh joylar soni: {son}")
